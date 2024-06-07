2bf6b14638dc909db9b601f634d0b9f872337feb
Madipally Naveen Kumar <madipallynaveen@gmail.com>
2024-06-07 17:23:08 +0530

    Initial Commit

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+    describe "dummy test" do
+        it "dummy test" do
+            expect(true).to eq(false)
+        end
+    end
+end
